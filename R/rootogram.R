d82f1962cdc6c1e3c321b2b9bbd79169c01b3296
Chris Brown <s2973410@staff.ad.griffith.edu.au>
2020-02-21 19:12:33 +1000

    Improve rootogram function

--- R/rootogram.R
+++ R/rootogram.R
@@ -1,10 +1,11 @@
 #' Create a rootogram from observed and predicted counts.
 #'
-#' @Usage rootogram(yobs, ypred, nbrks = 10, ...)
+#' @Usage rootogram(yobs, ypred, nbrks = 10, hanging = FALSE, ...)
 #'
 #' @param yobs numeric of observed counts
 #' @param ypred numeric of predicted counts
-#' @param nbrks number of x-axis breaks
+#' @param brks x-axis break points 
+#' @param hanging logical indicating whether to do hanging bars or not
 #' @param ... other arguments passed to plot.
 #'
 #' @return A rootogram.
@@ -17,6 +18,7 @@
 #' See Kleiber and Zeileis Visualizing Count Data
 #' Regression using Rootograms
 #' \url{{https://arxiv.org/pdf/1605.01311v1.pdf}
+#' Works best if break points are offset by 0.5 from integer values
 #'
 #'@examples 
 #' data(InsectSprays)
@@ -37,17 +39,26 @@
 #' @rdname rootogram
 #' @export
 
-rootogram <- function(yobs, ypred, nbrks = 10, ...){
-		 brks <- seq(0, ceiling(max(c(ypred, yobs))), length.out = nbrks)
-		  xwd <- diff(brks)[1]/2.1
-		 xy <- hist(ypred, breaks = brks, plot = F)
-		 xy2 <-hist(yobs, breaks = brks, plot = F)
-		ylwr <-  sqrt(xy$counts) - sqrt(xy2$counts)
-		ylim <- c(min(ylwr), sqrt(max(xy$counts)))
-
-		 plot(xy$mids, sqrt(xy$counts), lty = 3, col = 'red', type = 'b', ylim = ylim,
-		 xlab ='Count', ylab = 'sqrt(Frequency)', ...)
-		 rect(xy$mids - xwd, sqrt(xy$counts), xy$mids + xwd,ylwr, col = 'grey')
-		lines(xy$mids, sqrt(xy$counts), lty = 3, type = 'b', pch = 16, col = 'red')
-		abline(h = 0)
-		}
+rootogram <- function(yobs, ypred, brks = NA, hanging = FALSE, ...){
+  
+  if (is.na(brks)){
+    brks <- seq(0-0.5, ceiling(max(c(ypred, yobs)))+0.5, by = 1)
+  } 
+  xwd <- diff(brks)[1]/2.1
+  xy <- hist(ypred, breaks = brks, plot = F)
+  xy2 <- hist(yobs, breaks = brks, plot = F)
+  ylwr <-  sqrt(xy$counts) - sqrt(xy2$counts)
+  ylim <- c(min(ylwr), sqrt(max(xy$counts)))
+  xlim <- c(min(brks), max(brks))
+  plot(xy$mids, sqrt(xy$counts), lty = 3, col = 'red', type = 'b', ylim = ylim,
+       xlim = xlim, xlab ='Count', ylab = 'sqrt(Frequency)', ...)
+  
+  if (hanging){
+    rect(xy$mids - xwd, sqrt(xy$counts), xy$mids + xwd,ylwr, col = 'grey')
+    }else{
+      rect(xy$mids - xwd, sqrt(xy2$counts), xy$mids + xwd,0, col = 'grey')
+    }
+    
+  lines(xy$mids, sqrt(xy$counts), lty = 3, type = 'b', pch = 16, col = 'red')
+  abline(h = 0)
+}
